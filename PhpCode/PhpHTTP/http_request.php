c172ffd0a42cfe493dad2cc91615edad6406e0ba
shuai.zou <zoushuai518@gmail.com>
2014-06-25 15:04:25 +0800

    up

---
+++ PhpCode/PhpHTTP/http_request.php
@@ -0,0 +1,47 @@
+<?php
+
+//header('Content-type: application/javascript;charset=utf-8');
+/**
+ * Function Desc 模拟http请求
+ * Function name open
+ * @Param Param $uri
+ * @Param Param $type
+ * @Param Param $post_data
+ * @Returns Returns
+ */
+function http_request($uri, $type,$post_data= '') {
+	$user_agent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; CIBA)";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $uri);
+	//post
+	curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+	if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
+		curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+	}
+	switch ($type) {
+		case "GET":
+			curl_setopt($ch, CURLOPT_HTTPGET, 1);
+			break;
+		case "POST":
+			curl_setopt($ch, CURLOPT_POST, 1);
+			// json格式
+			//curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json;charset=utf-8"));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+			break;
+		case "PUT":
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+			break;
+		default:
+			curl_setopt($ch, CURLOPT_HTTPGET, 1);
+			break;
+	}
+
+	//默认ipv4
+	$output = curl_exec($ch);
+	curl_close($ch);
+	return $output;
+}
